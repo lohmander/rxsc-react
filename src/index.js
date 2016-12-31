9b5b2e5d85d8e75d5cdc5c24c833ad476d5b89bf
Hannes Lohmander <hannes@lohmander.me>
2016-12-31 14:22:25 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export function connect(container, stateMap = state => state) {
+    return component => React.createClass({
+        getInitialState: function() {
+            return stateMap(container.getInitialState());
+        },
+
+        componentDidMount: function() {
+            this.subscriber = container
+                .getObservable()
+                .map(stateMap)
+                .subscribe(state => this.setState(state))
+        },
+
+        componentWillUnmount: function() {
+            this.subscriber.unsubscribe();
+        },
+
+        render: function() {
+            return React.createElement(
+                component,
+                { ...this.props, ...this.state, actions$: container.actions$ },
+            );
+        },
+    });
+}
